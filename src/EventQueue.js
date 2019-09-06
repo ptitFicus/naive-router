0ea9313aad23f6f2e2ae7a79f19d74a17baa83e0
Benjamin Cavy <benjamin.cavy@gmail.com>
2019-09-07 01:02:31 +0200

    feat: NotFound routes

--- src/EventQueue.js
+++ src/EventQueue.js
@@ -1,22 +1,52 @@
 class EventQueue {
   constructor() {
+    this.notFound = false;
     this.queue = [];
+    this.notFoundQueue = [];
   }
 
   suscribe(callback) {
     this.queue.push(callback);
   }
 
+  suscribeNotFound(callback) {
+    this.notFoundQueue.push(callback);
+  }
+
   unsuscribe(callback) {
-    const index = this.queue.indexOf(callback);
+    this._unsuscribe(callback, this.queue);
+    this._unsuscribe(callback, this.notFoundQueue);
+  }
+
+  _unsuscribe(callback, queue) {
+    const index = queue.indexOf(callback);
     if (index === -1) {
       return;
     }
-    this.queue.splice(index, 1);
+    queue.splice(index, 1);
   }
 
   broadcast() {
-    this.queue.forEach(callback => callback());
+    const routeMatched = this.queue.reduce(
+      (acc, nextCallback) => nextCallback() || acc,
+      false
+    );
+
+    if (!routeMatched) {
+      this.notFound = true;
+      this.displayNotFoundRoutes();
+    } else if (this.notFound) {
+      this.notFound = false;
+      this.hideNotFoundRoutes();
+    }
+  }
+
+  hideNotFoundRoutes() {
+    this.notFoundQueue.forEach(callback => callback(true));
+  }
+
+  displayNotFoundRoutes() {
+    this.notFoundQueue.forEach(callback => callback(false));
   }
 }
 
